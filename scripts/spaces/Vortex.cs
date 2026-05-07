bc22496b9460379aa95005fd7f61c10312635436
lulu <lulu@lulusuki.me>
2026-05-07 16:55:25 +0900

    chore: linted vortex.cs

--- scripts/spaces/Vortex.cs
+++ scripts/spaces/Vortex.cs
@@ -5,78 +5,78 @@ namespace Spaces;
 
 public partial class Vortex : BaseSpace
 {
-	private SettingsProfile settings;
-	private SkinProfile skin;
+    private SettingsProfile settings;
+    private SkinProfile skin;
 
-	private Node3D squircles;
-	private StandardMaterial3D[] squircleMaterials;
-	private Color[] lastHitColors;
-	private Color squircleColorReset = new(0xffffff);
-	
-	public override void _Ready()
-	{
-		base._Ready();
+    private Node3D squircles;
+    private StandardMaterial3D[] squircleMaterials;
+    private Color[] lastHitColors;
+    private Color squircleColorReset = new(0xffffff);
 
-		settings = SettingsManager.Instance.Settings;
-		skin = SkinManager.Instance.Skin;
+    public override void _Ready()
+    {
+        base._Ready();
 
-		squircles = GetNode<Node3D>("Squircles");
-		
-		int squircleCount = squircles.GetChildCount();
+        settings = SettingsManager.Instance.Settings;
+        skin = SkinManager.Instance.Skin;
 
-		squircleMaterials = new StandardMaterial3D[squircleCount];
-		lastHitColors = new Color[squircleCount];
+        squircles = GetNode<Node3D>("Squircles");
 
-		// Start as if previous notes had been hit
-		var noteColors = (Color[])skin.NoteColors.Clone();
-		noteColors.Reverse();
+        int squircleCount = squircles.GetChildCount();
 
-		for (int i = 0; i < squircleCount; i++)
-		{
-			var squircle = squircles.GetChild<MeshInstance3D>(i);
-			var mesh = squircle.Mesh.Duplicate() as QuadMesh;
-			var	material = mesh.Material.Duplicate() as StandardMaterial3D;
-			
-			mesh.Material = material;
-			squircle.Mesh = mesh;
-			squircleMaterials[i] = material;
-			lastHitColors[i] = noteColors[i % noteColors.Length];
-		}
-		
-		// Wait for new runner to implement Hit signal connection here
-		// Update and push back lastHitColors order
-	}
+        squircleMaterials = new StandardMaterial3D[squircleCount];
+        lastHitColors = new Color[squircleCount];
 
-	public override void _Process(double delta)
-	{    
-		base._Process(delta);
-		
-		// nasty temporary
-		if (LegacyRunner.CurrentAttempt.LastHitColour != lastHitColors[0])
-		{
-			for (int i = lastHitColors.Length - 1; i > 0; i--)
-			{
-				lastHitColors[i] = lastHitColors[i - 1];
-			}
-			
-			lastHitColors[0] = LegacyRunner.CurrentAttempt.LastHitColour;
-		}
-		//
+        // Start as if previous notes had been hit
+        var noteColors = (Color[])skin.NoteColors.Clone();
+        noteColors.Reverse();
 
-		// Rotation
-		squircles.Rotation = Vector3.Forward * (Time.GetTicksMsec() / 8000f);
-		
-		// Hit FX
-		for (int i = 0; i < squircleMaterials.Length; i++)
-		{
-			var color = squircleColorReset;
+        for (int i = 0; i < squircleCount; i++)
+        {
+            var squircle = squircles.GetChild<MeshInstance3D>(i);
+            var mesh = squircle.Mesh.Duplicate() as QuadMesh;
+            var material = mesh.Material.Duplicate() as StandardMaterial3D;
 
-			if (settings.SpaceHitEffects)
-			{
-				color = squircleMaterials[i].AlbedoColor.Lerp(lastHitColors[i], Math.Min(1, (float)delta * 6));
-			}
-			
-			squircleMaterials[i].AlbedoColor = color;
-		}
-	}
+            mesh.Material = material;
+            squircle.Mesh = mesh;
+            squircleMaterials[i] = material;
+            lastHitColors[i] = noteColors[i % noteColors.Length];
+        }
+
+        // Wait for new runner to implement Hit signal connection here
+        // Update and push back lastHitColors order
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+
+        // nasty temporary
+        if (LegacyRunner.CurrentAttempt.LastHitColour != lastHitColors[0])
+        {
+            for (int i = lastHitColors.Length - 1; i > 0; i--)
+            {
+                lastHitColors[i] = lastHitColors[i - 1];
+            }
+
+            lastHitColors[0] = LegacyRunner.CurrentAttempt.LastHitColour;
+        }
+        //
+
+        // Rotation
+        squircles.Rotation = Vector3.Forward * (Time.GetTicksMsec() / 8000f);
+
+        // Hit FX
+        for (int i = 0; i < squircleMaterials.Length; i++)
+        {
+            var color = squircleColorReset;
+
+            if (settings.SpaceHitEffects)
+            {
+                color = squircleMaterials[i].AlbedoColor.Lerp(lastHitColors[i], Math.Min(1, (float)delta * 6));
+            }
+
+            squircleMaterials[i].AlbedoColor = color;
+        }
+    }
 }
