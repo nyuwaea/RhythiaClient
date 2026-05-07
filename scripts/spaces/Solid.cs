dcb298c5d18cfd5f794212d5c4b6ee3023cc69b2
lulu <lulu@lulusuki.me>
2026-05-07 18:23:28 +0900

    feat: solid space
    .

---
+++ scripts/spaces/Solid.cs
@@ -0,0 +1,81 @@
+using Godot;
+
+namespace Spaces;
+
+public partial class Solid : BaseSpace
+{
+    private Color defaultEnvironmentColor;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+
+        defaultEnvironmentColor = WorldEnvironment.Environment.BackgroundColor;
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+
+        if (Playing)
+        {
+            updateColor(NoteHitColor);
+        }
+        else
+        {
+            Viewport viewport = GetViewport();
+            Vector2 centerOffset = viewport.GetMousePosition() - viewport.GetVisibleRect().Size / 2;
+
+            Camera.Position = new Vector3(centerOffset.X, centerOffset.Y, 0) / 40000;
+        }
+    }
+
+    public override void UpdateMap(Map map)
+    {
+        base.UpdateMap(map);
+
+        Color color = defaultEnvironmentColor;
+
+        if (!Playing && Cover != null)
+        {
+            Image coverImage = Cover.GetImage();
+
+            if (coverImage.IsCompressed())
+            {
+                return;
+            }
+
+            Vector3 avg = Vector3.Zero;
+            int pixelCount = 0;
+
+            for (int x = 0; x < coverImage.GetWidth(); x++)
+            {
+                for (int y = 0; y < coverImage.GetHeight(); y++)
+                {
+                    Color pixel = coverImage.GetPixel(x, y);
+
+                    if (pixel.A == 0)
+                    {
+                        continue;
+                    }
+
+                    avg += new Vector3(pixel.R, pixel.G, pixel.B);
+                    pixelCount++;
+                }
+            }
+
+            avg /= pixelCount;
+            color = new(avg.X, avg.Y, avg.Z);
+        }
+
+        updateColor(color);
+    }
+
+    private void updateColor(Color color)
+    {
+        Color darkened = color.Darkened(0.9f);
+
+        WorldEnvironment.Environment.BackgroundColor = Playing ? darkened : (Cover != null ? darkened : defaultEnvironmentColor);
+    }
+}
