6176ed6993000fad1391c26c154e7ddf277b3bd1
fogsaturate <brangaming95@gmail.com>
2026-05-18 23:15:48 -0400

    if temp map exists, no need to record replay (this also horribly errored the runner)

--- scripts/game/managers/ReplayManager.cs
+++ scripts/game/managers/ReplayManager.cs
@@ -35,7 +35,7 @@ public partial class ReplayManager : Node
     {
         var settings = attempt.Settings;
 
-        if (!settings.RecordReplays) return;
+        if (!settings.RecordReplays || Rhythia.Instance.TextFilePath != null) return;
 
         ReplayPath = $"{Constants.USER_FOLDER}/replays/{attempt.ID}.phxr";
 
